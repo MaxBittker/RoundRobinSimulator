a03ec6be9e89ba8e8d3d820987683faeaf3b5207
max bittker <maxbittker@gmail.com>
2015-06-11 15:32:11 -0400

    initial commit, no logic

---
+++ logic.js
@@ -0,0 +1,11 @@
+var initialStatus = { 'teams':[
+    {'team': "SKT", 'won': 5, 'lost': 0, 'mwon': 15, 'mlost': 6},
+    {'team': "KTR", 'won': 3, 'lost': 2, 'mwon': 13, 'mlost': 8},
+    {'team': "SBN", 'won': 3, 'lost': 2, 'mwon': 11, 'mlost': 8},
+    {'team': "MVP", 'won': 3, 'lost': 2, 'mwon': 12, 'mlost': 11},
+    {'team': "CJE", 'won': 2, 'lost': 3, 'mwon': 10, 'mlost': 11},
+    {'team': "JGW", 'won': 2, 'lost': 3, 'mwon': 9, 'mlost': 11},
+    {'team': "SMG", 'won': 2, 'lost': 3, 'mwon': 7, 'mlost': 10},
+    {'team': "PRM", 'won': 0, 'lost': 5, 'mwon': 3, 'mlost': 15}
+]};
+for()
